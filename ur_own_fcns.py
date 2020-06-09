6809f42729bbfed4aa9ecd3d31acc2d1e7e56522
Joel Duran <duranjoel.a@gmail.com>
2020-06-09 10:17:26 -0400

    your own fcns

---
+++ ur_own_fcns.py
@@ -0,0 +1,13 @@
+def hello():
+    print('Hello')
+
+hello()
+hello()
+
+def hello_1(name):
+    print('hello ' + name)
+
+hello_1('Joel')
+
+# print arguments
+print('joel', 'duran', 'abreu', sep='_')
